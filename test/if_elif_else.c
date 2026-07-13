3398e5a77c1723c5a208abb3ad109d5b585ca90f
Revan Aprian <renzx430@gmail.com>
2026-07-13 10:04:20 +0000

    remove binary

---
+++ test/if_elif_else.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char** argv)
+{
+    int x = 15;
+    if (x > 10) {
+    printf("%d\n", 1);
+    }
+    else if (x > 5) {
+    printf("%d\n", 2);
+    }
+    else {
+    printf("%d\n", 3);
+    }
+    return 0;
+}
